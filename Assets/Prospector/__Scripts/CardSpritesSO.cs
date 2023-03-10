3ecda539edb49658adf1216c205561e81ac952eb
cab557 <102686043+cab557@users.noreply.github.com>
2023-03-09 20:00:05 -0500

    Update CardSpritesSO.cs

--- Assets/Prospector/__Scripts/CardSpritesSO.cs
+++ Assets/Prospector/__Scripts/CardSpritesSO.cs
@@ -21,4 +21,60 @@ public class CardSpritesSO : ScriptableObject
     [Header("Pip Sprites")]
     public Sprite[] faceSprites;
     public Sprite[] rankSprites;
+
+    private static CardSpritesSO S;
+    private static Dictionary<char, Sprite> SUITS { get; private set; }
+
+    public void Init()
+    {
+        INIT_STATISTICS(this);
+    }
+    /// <summary>
+    /// Initializes the static elements of CardSpriteSO
+    /// </summary>
+    /// <param name="cSSO">CardSpriteSO to be assigned to the Singleton S</param>
+    static void INIT_STATICS(CardSpritesSO cSSO)
+    {
+        if (S != null)
+        {
+            Debug.LogError("CardSpritesSO.S can't be a 2nd time!");
+            return;
+        }
+        S = cSSO;
+        SUITS = new Dictionary<char, Sprite>()
+        {
+            {"C", S.suitClub },
+            {"D", S.suitDiamond },
+            {"H", S.suitHeart },
+            {"S", S.suitSpade }
+        };
+    }
+
+    public static Sprite[] RANKS
+    {
+        get { return S.rankSprites; }
+    }
+
+    /// <summary
+    /// Searches S.faceSprites for the one with the right name
+    /// </summary>
+    /// <param name="name">The name to search for</param>
+    /// <returns>A face Sprite</returns>
+    public static Sprite GET_FACE(string name)
+    {
+        foreach (Sprite spr in S.faceSprites)
+        {
+            if (spr.name == name) return spr;
+        }
+        return null;
+    }
+    public static Sprite BACK
+    {
+        get { return S.cardBack; }
+    }
+
+    public static void RESET()
+    {
+        S = null;
+    }
 }
